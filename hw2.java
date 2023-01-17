79ca000d8c02dc343dcdae783d7386fcd0d5ebb0
jtsyganov <jtsyganov@gmail.com>
2023-01-17 23:06:18 +0200

    Class 9

--- hw2.java
+++ hw2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class hw2 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             System.out.println("Apply for a credit card");
             String userResponce= scanner.next();
             if (userResponce.equalsIgnoreCase("yes")) {
